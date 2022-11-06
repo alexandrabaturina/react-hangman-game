33fff4169c722ed9f99fb5acc4c911f500d0b4e7
alexandrabaturina <alexandra.v.baturina@gmail.com>
2022-11-06 15:09:19 -0800

    feat: display hangman word

--- src/components/HangmanWord.js
+++ src/components/HangmanWord.js
@@ -1,3 +1,30 @@
 export const HangmanWord = () => {
-	return <div>Hangman Word</div>
+	const word = "test"
+	const guessedLetters = ["e"]
+	return (
+		<div
+			style={{
+				display: "flex",
+				gap: ".25em",
+				fontSize: "6rem",
+				fontWeight: "bold",
+				textTransform: "uppercase",
+				fontFamily: "monospace",
+			}}
+		>
+			{word.split("").map((letter, index) => (
+				<span style={{ borderBottom: ".1em solid black" }} key={index}>
+					<span
+						style={{
+							visibility: guessedLetters.includes(letter)
+								? "visible"
+								: "hidden",
+						}}
+					>
+						{letter}
+					</span>
+				</span>
+			))}
+		</div>
+	)
 }
